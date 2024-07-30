e2c1ac1d0805076380356843f670d778202a11fc
Kalpashreedas <144610757+Kalpashreedas@users.noreply.github.com>
2024-07-30 14:05:42 +0530

    Add files via upload

---
+++ Week_1_Exercises/FactoryMethodPatternExample/TestFactoryMethodPattern.java
@@ -0,0 +1,26 @@
+package FactoryMethodPatternExample;
+
+public class TestFactoryMethodPattern {
+    public static void main(String[] args) {
+        // Create a Word document
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDocument = wordFactory.createDocument();
+        wordDocument.open();
+        wordDocument.save();
+        wordDocument.close();
+
+        // Create a PDF document
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDocument = pdfFactory.createDocument();
+        pdfDocument.open();
+        pdfDocument.save();
+        pdfDocument.close();
+
+        // Create an Excel document
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excelDocument = excelFactory.createDocument();
+        excelDocument.open();
+        excelDocument.save();
+        excelDocument.close();
+    }
+}
